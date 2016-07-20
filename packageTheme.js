afbf05fcb47bcb1116fb34398c867d9d89ca663f
Tianze Zhao <zen.zhao88@gmail.com>
2016-07-20 18:01:47 +0800

    init project

---
+++ packageTheme.js
@@ -0,0 +1,249 @@
+#!/usr/bin/env node
+
+// file system
+var fs = require('fs');
+// shell tools
+var shell = require('shelljs');
+// underscore
+var _ = require('underscore');
+var zipFolder = require('zip-folder');
+var child_process = require('child_process');
+
+var resourcePath = '../SolitaireResource/图片资源/UI/新主题';
+var outPutPath = '../SolitaireClient/Proj.android-studio/theme/src/main';
+var tmpRootPath = 'tmp';
+var tmp2RootPath = 'tmp2';
+var zipRootPath = 'zip';
+var projScrRootPath = '../SolitaireClient/Proj.android-studio/theme/src/main';
+var packageFilePath = '../SolitaireClient/scr/package.json';
+var SolitaireClient_Path = '../SolitaireClient'
+var SolitaireThemePakeage_Path = '../SolitaireThemePakeage'
+
+var exists = function (path) {
+  try {
+    return fs.statSync(path,fs.F_OK);
+  } catch (error) {
+    return false;
+  }
+}
+
+var copyFile = function (src,dist) {
+  // console.log('复制文件：'+src+' 到 '+dist);
+  var isExists = exists(src);
+  if (isExists){
+    // fs.createReadStream(src).pipe(fs.createWriteStream(dist));
+    shell.exec('cp -f '+src+' '+dist);
+  }
+}
+
+var rmDir = function (path) {
+  var isExists = exists(path);
+  if (isExists) {
+    shell.exec('rm -rf '+path);//强制删除整个tmp文件夹
+  }
+}
+
+var copyDir = function (src,dist) {
+  var isExists = exists(src);
+  if (isExists) {
+    var distState = exists(dist);
+    if (!distState){
+      fs.mkdirSync(dist);//创建目标文件夹
+    }
+    var files = fs.readdirSync(src);
+    files.forEach(function (file) {
+      var _src = src + '/' + file;
+      var _dist = dist + '/' + file;
+      var state = exists(_src);
+      if (state && state.isFile()) {
+        copyFile(_src,_dist);
+      }else if (state && state.isDirectory()) {
+        copyDir(_src,_dist);
+      }
+    })	
+  }
+}
+
+var readThemeJsonFile = function (fileName) {
+  var filePath = resourcePath + '/' + fileName;
+  var files = fs.readdirSync(filePath);
+  var jsonFileName = "theme_"+fileName+".json";
+  var index = _.indexOf(files,jsonFileName);
+  if (index === -1) {
+    return undefined;
+  }
+  var jsonFilePath = filePath+'/'+jsonFileName;
+  var themeConfig = JSON.parse(fs.readFileSync(jsonFilePath));
+  return themeConfig;
+}
+
+var Operation1 = function (fileName) {
+    if (fileName === '.DS_Store'){
+      return;
+    }
+    var tmpFilePath = tmpRootPath+'/'+fileName;
+    var UI_ResourcesPath = tmpFilePath+'/UI_Resources';
+    fs.mkdirSync(tmpFilePath);
+    fs.mkdirSync(UI_ResourcesPath);
+    console.log('开始遍历文件：'+fileName);
+    var filePath = resourcePath + '/' + fileName;
+    var themeConfig = readThemeJsonFile(fileName);
+    if (!themeConfig){
+      return console.error('在%s文件夹下，没有找到可用的json配置文件',fileName);
+    }
+    var textureName = themeConfig.cardPlist;
+    //生成大图
+    shell.exec('TexturePacker --sheet ' + UI_ResourcesPath + '/' + textureName + '.png --data ' + UI_ResourcesPath + '/' + textureName + '.plist ' +filePath + '/UI_CardResources/*png');
+    var jsonFileName = "theme_"+fileName+".json";
+    //拷贝json文件
+    copyFile(filePath+'/'+jsonFileName,UI_ResourcesPath+'/'+jsonFileName);
+    //拷贝color文件
+    var colorFileName = "color"+themeConfig.faceIndex+'.cardcolor';
+    copyFile(filePath+'/'+colorFileName,UI_ResourcesPath+'/'+colorFileName);
+    //拷贝paticle文件夹
+    var paticleDirPath = filePath+'/particle';
+    copyDir(paticleDirPath,tmpFilePath+'/particle');
+    //拷贝theme文件夹
+    var themeDirPath = filePath+'/theme';
+    copyDir(themeDirPath,UI_ResourcesPath+"/theme");
+    //拷贝cardstyle文件夹
+    var cardstyleDirPath = filePath+'/cardstyle';
+    copyDir(cardstyleDirPath,UI_ResourcesPath+"/cardstyle");
+    // //进行文件加密
+    var shellEx = '$QUICK_COCOS2DX_ROOT/bin/pack_files.sh -i ' + tmpFilePath + ' -o ' + tmp2RootPath + '/' + fileName + ' -ek 8VVTJ-UC2R1 -es XXTEA -x .json,.cardcolor';
+    console.log(shellEx);
+    shell.exec(shellEx);
+}
+
+var Operation2 = function(floders,index,callback) {
+  if (floders.length <= index) {
+    callback();
+    return;
+  }
+  //生成.zip操作
+  if (fileName === '.DS_Store'){
+    Operation2(floders,index+1,callback);
+  }else{
+    var fileName = floders[index]; 
+    var tmpFilePath = tmp2RootPath+'/'+fileName;
+    zipFolder(tmpFilePath,zipRootPath+'/'+fileName+'.aac',function(error) {
+      if (error){
+        console.log(fileName + '打包zip失败：'+error);
+        Operation2(floders,index+1,callback);
+      }else{
+        //修改后缀为.aac
+        // fs.renameSync(oldPath, newPath)
+        Operation2(floders,index+1,callback);
+      }
+    });
+  }
+}
+
+var supportLan = ['ar','de','es','fr','id','it','ja','ko','nl','pt','ru','tr','zh-rCN','zh-rHK','zh-rTW'];
+
+var changeAppName = function (file,themeConfig) {
+  var resDirPath = projScrRootPath + '/res_' + themeConfig.key;
+  var files = fs.readdirSync(resDirPath);
+  changeXML(resDirPath+'/values/strings.xml',themeConfig.appName);
+  supportLan.forEach(function(lan) {
+    changeXML(resDirPath+'/values-'+lan+'/values-'+lan+'.xml',themeConfig.appName);
+  }, this);
+}
+
+var changeXML = function (xmlPath,appName) {
+  var stringsXml = fs.readFileSync(xmlPath,'utf8');
+  stringsXml = stringsXml.replace(/Soccer Theme/,appName);
+  fs.writeFileSync(xmlPath,stringsXml,'utf8');
+}
+
+//准备工程文件
+var Operation3 = function(files) {
+  console.log('开始准备工程文件');
+  var packageJson = JSON.parse(fs.readFileSync(packageFilePath));
+  var newThemes = {}
+  files.forEach(function(file) {
+    var themeConfig = readThemeJsonFile(file);
+    if (themeConfig) {
+      var packageKey = "com.born2play.solitaire.theme."+themeConfig.key;
+      var resDirName = 'res_'+themeConfig.key;
+      var assetsDirName = 'assets_'+themeConfig.key;
+      //创建相关的文件夹
+      var resDirPath = projScrRootPath + '/' + resDirName;
+      var assetsDirPath = projScrRootPath + '/' + assetsDirName;
+      console.log(file+'开始准备asset和res文件夹');
+      fs.mkdirSync(assetsDirPath);
+      copyDir(projScrRootPath+'/resDefault',resDirPath);
+      //拷贝zip文件到assets下
+      var zipFilePath = zipRootPath+'/'+file+'.aac';
+      copyFile(zipFilePath,assetsDirPath+'/'+file+'.aac');
+      //拷贝bg.png到res/drawable
+      var bgPngFilePath = resourcePath+'/'+file+'/bg.png';
+      copyFile(bgPngFilePath,resDirPath+'/drawable/bg.png');
+      //拷贝icon到res文件下
+      var iconDirPath = resourcePath+'/'+file+'/icon';
+      var iconDirs = fs.readdirSync(iconDirPath)
+      iconDirs.forEach(function(iconDir) {
+        if (iconDir.indexOf('mipmap') === 0){
+          copyDir(iconDirPath + '/' + iconDir, resDirPath + '/' + iconDir);
+        }
+      }, this);
+      //修改res下value文件的appName
+      changeAppName(file,themeConfig);
+      //
+      newThemes[packageKey] = {
+        name:themeConfig.key,
+        resDir:resDirName,
+        assetsDir:assetsDirName
+      }
+    }
+  }, this);
+  console.log('修改工程配置文件packge.json');
+  packageJson.theme = newThemes;
+  fs.writeFileSync(packageFilePath,JSON.stringify(packageJson),'utf8');
+}
+
+var rmProjectResource = function() {
+  var floders = fs.readdirSync(projScrRootPath);
+  for (var index = 0; index < floders.length; index++) {
+    var floderName = floders[index];
+    var assetsIndex = floderName.indexOf('assets_');
+    var resIndex = floderName.indexOf('res_');
+    if (assetsIndex != -1 || resIndex != -1) {
+      rmDir(projScrRootPath + '/' + floderName);
+    }
+  }
+}
+
+var Operation4 = function () {
+  shell.exec('cd ../SolitaireClient');
+  // shell.exec('./buildAPK.sh');
+  // shell.exec('cd '+ SolitaireThemePakeage_Path);
+}
+
+var OperationBegin = function() {
+    //1.删除原有的临时文件，创建新的文件夹
+    rmDir(tmpRootPath);
+    rmDir(tmp2RootPath);
+    rmDir(zipRootPath);
+    fs.mkdirSync(tmpRootPath);
+    fs.mkdirSync(tmp2RootPath);
+    fs.mkdirSync(zipRootPath);
+    //读取资源文件夹
+    var files = fs.readdirSync(resourcePath);
+    var index = _.indexOf(files,'.DS_Store')
+    if (index != -1){
+      files.splice(index,1);
+    }
+    console.log(files);
+    files.forEach(function(file) {
+      Operation1(file);
+    }, this);
+    
+
+    Operation2(files,0,function() {
+      rmProjectResource();
+      Operation3(files);
+    })
+}
+
+OperationBegin();

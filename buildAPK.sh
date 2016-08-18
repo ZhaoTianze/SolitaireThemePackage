74c1df7cc63f69446321ae67a5b322dd7479474e
yfc <yangfuchao@talefun.us>
2016-08-18 13:00:00 +0800

    增加脚本

---
+++ buildAPK.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source ~/.bash_profile
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APP_ROOT="$DIR"
+APP_STUDIO_ROOT="$DIR/SolitaireTheme"
+
+cd $APP_ROOT
+
+packageTheme
+
+cd $APP_STUDIO_ROOT
+
+gradle assembleRelease		
+
+cd $APP_ROOT
